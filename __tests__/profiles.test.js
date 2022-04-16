914a26c7b3b5aee22953aaeebd698b3638799ff2
forestheims <forestheims@gmail.com>
2022-04-16 15:02:53 -0700

    create profile route created and passing test

--- __tests__/profiles.test.js
+++ __tests__/profiles.test.js
@@ -2,6 +2,9 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const agent = request.agent(app);
 
 describe('geo-tone-backend routes', () => {
   beforeEach(() => {
@@ -18,9 +21,17 @@ describe('geo-tone-backend routes', () => {
     avatar: 'url',
   };
 
+  const mockUser = {
+    username: 'mockusername',
+    password: 'mockpassword',
+  };
+
   it('creates a profile', async () => {
-    const res = await request(app).post('/api/v1/profiles').send(mockProfile);
-    // not complete
-    expect(res.body).toEqual({});
+    await UserService.create(mockUser);
+    await agent.post('/api/v1/users/sessions').send(mockUser);
+    // expect(res1.body).toEqual({ message: 'Succussfully signed in!' });
+    const res = await agent.post('/api/v1/profiles').send(mockProfile);
+    console.log('res.body', res.body);
+    expect(res.body).toEqual({ profileId: expect.any(String), ...mockProfile });
   });
 });
