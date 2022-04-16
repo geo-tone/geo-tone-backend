efa5d140b527e550a4d83b6919561dfc4b60a4d4
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-16 11:30:59 -0700

    Set up creating a user test

---
+++ __tests__/users.test.js
@@ -0,0 +1,29 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const agent = request.agent(app);
+
+describe('user routes test', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  const mockUser = {
+    username: 'mockusername',
+    password: 'mockpassword',
+  };
+
+  // CREATE A NEW USER
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    expect(res.body).toEqual({
+      user_id: expect.any(String),
+      username: 'mockusername',
+    });
+  });
+});
