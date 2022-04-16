1c83cf697392396014be0549ea9c6bd2afc460e9
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-04-16 13:02:28 -0700

    TDD for signing in a user working with cookies and jwt working

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const User = require('../lib/models/User');
 const UserService = require('../lib/services/UserService');
 
 const agent = request.agent(app);
