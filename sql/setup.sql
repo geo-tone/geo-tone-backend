a7f5a8607096ce214ec74ea08a501243d8b3dade
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-04-18 20:38:09 -0700

    add username to profiles table, readability update

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,25 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS projects CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS channels CASCADE;
 DROP TABLE IF EXISTS profiles CASCADE;
+DROP TABLE IF EXISTS channels CASCADE;
+DROP TABLE IF EXISTS projects CASCADE;
 
 CREATE TABLE users(
-    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username TEXT NOT NULL,
-    password_hash TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE projects(
-  project_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE profiles(
+  profile_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT NOT NULL,
-  title TEXT NOT NULL,
-  volume SMALLINT NOT NULL,
-  bpm SMALLINT NOT NULL,
-  channels TEXT [] NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
+  username TEXT NOT NULL,
+  bio TEXT NOT NULL,
+  avatar TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (username) REFERENCES users(username)
   ON DELETE CASCADE
 );
 
@@ -34,11 +34,13 @@ CREATE TABLE channels(
   ON DELETE CASCADE
 );
 
-CREATE TABLE profiles(
-  profile_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+CREATE TABLE projects(
+  project_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT NOT NULL,
-  bio TEXT NOT NULL,
-  avatar TEXT NOT NULL,
+  title TEXT NOT NULL,
+  volume SMALLINT NOT NULL,
+  bpm SMALLINT NOT NULL,
+  channels TEXT [] NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(user_id)
   ON DELETE CASCADE
 );
@@ -46,11 +48,11 @@ CREATE TABLE profiles(
 INSERT INTO users(username, password_hash)
 VALUES ('space-lady', '123456'); 
 
-INSERT INTO projects(title, volume, bpm, channels, user_id)
-VALUES ('our seeded project', 0, 90, ARRAY ['{ "id": 0, "type": "synth", "osc": "sine", "steps": [null, null, null, null, null, null, null, null], "volume": -5, "reverb": 0.5 }'], '1'); 
+INSERT INTO profiles(user_id, username, bio, avatar)
+VALUES ('1', 'space-lady', 'paragon of outsider music, inspiration of tones', 'https://media2.fdncms.com/portmerc/imager/u/original/19330747/1505926068-music-ttd-paveladyspacelady-terriloewenthal-2.jpg'); 
 
 INSERT INTO channels(project_id, title, instrument, fx, steps)
 VALUES ('1', 'channel title', '{ "osc": "sine" }', '{ "reverb": 0.01 }', '{ "C4", "D4" }');
 
-INSERT INTO profiles(user_id, bio, avatar)
-VALUES ('1', 'paragon of outsider music, inspiration of tones', 'https://media2.fdncms.com/portmerc/imager/u/original/19330747/1505926068-music-ttd-paveladyspacelady-terriloewenthal-2.jpg'); 
+INSERT INTO projects(title, volume, bpm, channels, user_id)
+VALUES ('our seeded project', 0, 90, ARRAY ['{ "id": 0, "type": "synth", "osc": "sine", "steps": [null, null, null, null, null, null, null, null], "volume": -5, "reverb": 0.5 }'], '1'); 
