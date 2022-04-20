73892011b74ebaa2e85d2950cd8c315a7c88ac04
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-04-19 20:25:27 -0700

    update to refute unwanted prettier formatting

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
 DROP TABLE IF EXISTS users CASCADE;
-
 DROP TABLE IF EXISTS profiles CASCADE;
-
-DROP TABLE IF EXISTS channels CASCADE;
-
 DROP TABLE IF EXISTS projects CASCADE;
+DROP TABLE IF EXISTS channels CASCADE;
 
 CREATE TABLE users(
   user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -14,6 +12,11 @@ CREATE TABLE users(
   password_hash TEXT NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO
+  users(username, password_hash)
+VALUES
+  ('space-lady', '123456');
 
 CREATE TABLE profiles(
   profile_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -25,6 +28,16 @@ CREATE TABLE profiles(
   FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
 );
 
+INSERT INTO
+  profiles(user_id, username, bio, avatar)
+VALUES
+  (
+    '1',
+    'space-lady',
+    'paragon of outsider music, inspiration of tones',
+    'https://media2.fdncms.com/portmerc/imager/u/original/19330747/1505926068-music-ttd-paveladyspacelady-terriloewenthal-2.jpg'
+  );
+
 CREATE TABLE projects(
   project_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT NOT NULL,
@@ -35,6 +48,11 @@ CREATE TABLE projects(
   FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
+INSERT INTO
+  projects(user_id)
+VALUES
+  ('1'); 
+
 CREATE TABLE channels(
   channel_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   project_id BIGINT NOT NULL,
@@ -44,27 +62,6 @@ CREATE TABLE channels(
   steps TEXT [] NOT NULL,
   FOREIGN KEY (project_id) REFERENCES projects(project_id) ON DELETE CASCADE
 );
-
-INSERT INTO
-  users(username, password_hash)
-VALUES
-  (' space-lady ', ' 123456 ');
-
-INSERT INTO
-  profiles(user_id, username, bio, avatar)
-VALUES
-  (
-    ' 1 ',
-    ' space-lady ',
-    ' paragon of outsider music,
-  inspiration of tones ',
-    ' https: / / media2.fdncms.com / portmerc / imager / u / original / 19330747 / 1505926068 - music - ttd - paveladyspacelady - terriloewenthal -2.jpg '
-  );
-
-INSERT INTO
-  projects(user_id)
-VALUES
-  (' 1 '); 
 
 INSERT INTO channels(project_id, title, instrument, fx, steps)
 VALUES (' 1 ', ' channel title ', ' { "osc": "sine" } ', ' { "reverb": 0.01 } ', ' { "C4",
